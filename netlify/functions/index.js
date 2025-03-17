8bd7779cbb7aac757475394cb11cbc2e89a5f15d
Alex Oliveira <contatoalexandergv@gmail.com>
2025-03-17 18:39:16 -0300

    try to fix netlify

--- netlify/functions/index.js
+++ netlify/functions/index.js
@@ -7,8 +7,14 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-app.get("/", (req, res) => {
-  res.send("Teste!");
+app.get("/", async (req, res) => {
+  console.log("Teste!");
+  return {
+    statusCode: 200,
+    body: JSON.stringify({
+      message: "This is what will be returned!",
+    }),
+  };
 });
 
 module.exports.handler = serverless(app);
