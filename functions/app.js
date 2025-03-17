206b003285a36d9b38ac4b863ecce74051b3ffe3
Alex Oliveira <contatoalexandergv@gmail.com>
2025-03-17 18:17:52 -0300

    try fix netlify

--- functions/app.js
+++ functions/app.js
@@ -1,18 +1,14 @@
 const express = require("express");
 const cors = require("cors");
-const { PrismaClient } = require("@prisma/client");
 const serverless = require("serverless-http");
 
-const prisma = new PrismaClient();
 const app = express();
-const router = express.Router();
 
 app.use(cors());
 app.use(express.json());
 
-app.get("/", async (req, res) => {
+app.get("/", (req, res) => {
   res.send("Teste!");
 });
 
-app.use("/", router);
 module.exports.handler = serverless(app);
